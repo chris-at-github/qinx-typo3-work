94f344afbb2e0a87043059c0315e0d30e9505ca0
Chris <pschorr.christian@gmail.com>
2016-08-04 06:29:20 +0200

    Fix mm loading cards of containers

--- Classes/Domain/Repository/CardRepository.php
+++ Classes/Domain/Repository/CardRepository.php
@@ -1,31 +1,30 @@
 <?php
 namespace Qinx\Qxwork\Domain\Repository;
 
-
-	/***************************************************************
-	 *
-	 *  Copyright notice
-	 *
-	 *  (c) 2016 Christian Pschorr <qinx.me>
-	 *
-	 *  All rights reserved
-	 *
-	 *  This script is part of the TYPO3 project. The TYPO3 project is
-	 *  free software; you can redistribute it and/or modify
-	 *  it under the terms of the GNU General Public License as published by
-	 *  the Free Software Foundation; either version 3 of the License, or
-	 *  (at your option) any later version.
-	 *
-	 *  The GNU General Public License can be found at
-	 *  http://www.gnu.org/copyleft/gpl.html.
-	 *
-	 *  This script is distributed in the hope that it will be useful,
-	 *  but WITHOUT ANY WARRANTY; without even the implied warranty of
-	 *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	 *  GNU General Public License for more details.
-	 *
-	 *  This copyright notice MUST APPEAR in all copies of the script!
-	 ***************************************************************/
+/***************************************************************
+ *
+ *  Copyright notice
+ *
+ *  (c) 2016 Christian Pschorr <qinx.me>
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
 
 /**
  * The repository for Cards
@@ -54,7 +53,7 @@ class CardRepository extends \TYPO3\CMS\Extbase\Persistence\Repository {
 				$options['container'] = $options['container']->getUid();
 			}
 
-			$matches[] = $query->equals('container', $options['container']);
+			$matches[] = $query->contains('container', $options['container']);
 		}
 
 		if(empty($matches) === false) {
